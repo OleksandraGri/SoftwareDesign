4c2ee800ff2f99f1c212aa1936dec5540a14841d
OleksandraGri <ipz235_goo@student.ztu.edu.ua>
2025-05-18 19:07:28 +0300

    Lifecycle Hooks
    Додано хуки життєвого циклу елементів (OnCreated, OnInserted і тд,) 
    Тепер елементи можуть реагувати на події створення, додавання до DOM і тд.

--- lab3/task5_composite.py
+++ lab3/task5_composite.py
@@ -1,26 +1,58 @@
 class LightNode:
-    def __init__(self, tag=None):
-        self.tag = tag
+    def __init__(self):
         self.children = []
 
-    def add_child(self, child):
-        self.children.append(child)
-
-    def render(self):
+    def on_created(self):
         pass
 
-class LightElementNode(LightNode):
-    def __init__(self, tag, is_block=True, is_self_closing=False):
-        super().__init__(tag)
-        self.is_block = is_block
-        self.is_self_closing = is_self_closing
+    def on_inserted(self):
+        pass
+
+    def on_removed(seld):
+        pass
+
+    def on_styles_applied(self):
+        pass
+
+    def on_text_rendered(self):
+        pass
+        
+    def add_child(self, child):
+        self.children.append(child)
+        child.on_inserted()
 
     def render(self):
+        self.on_created()
+        self._render_impl()
+        self.on_text_rendered()
+
+    def _render_impl(self):
+        raise NotImplementedError()
+
+class LightElementNode(LightNode):
+    def __init__(self, tag, is_block=True):
+        super().__init__(tag)
+        self.is_block = is_block
+        self.tag = tag
+        self.styles = {}
+        self.classes = []
+
+    def add_style(self, name, value):
+        self.styles[name] = value
+        self.on_styles_applied()
+
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+        self.on_class_list_applied()
+
+    def on_class_list_applied(self):
+        print(f"Class applied to {self.tag}")
+        
+    def _render_impl(self):
         print(f"<{self.tag}>")
         for child in self.children:
             child.render()
-        if not self.is_self_closing:
-            print(f"</{self.tag}>")
+        print(f"</{self.tag}>")
 
 class LightTextNode(LightNode):
     def __init__(self, text):
